49f09a07744ea6535dfc6deeb00bf54d5476586c
rodrrigodev <rodrigosanyy@gmail.com>
2023-04-05 11:58:28 -0300

    profile almost finished

--- src/pages/home/components/profileCard/index.tsx
+++ src/pages/home/components/profileCard/index.tsx
@@ -11,42 +11,76 @@ import {
   UserNameAndGit,
 } from './styles'
 import { useTheme } from 'styled-components'
+import { useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+
+interface ApiProps {
+  name: string
+  login: string
+  bio: string
+  company?: string
+  avatar_url: string
+  followers: number
+}
+
+interface UserInfoProps {
+  name: string
+  login: string
+  bio: string
+  company?: string
+  avatar: string
+  followers: number
+}
 
 export function ProfileCard() {
   const theme = useTheme()
+
+  const [userInfo, setUserInfo] = useState<UserInfoProps>()
+
+  async function getUserInfo() {
+    const response: ApiProps = await (await api.get('/users/rodrrigodev')).data
+
+    const {
+      avatar_url: avatar,
+      bio,
+      followers,
+      login,
+      name,
+      company,
+    } = response
+    setUserInfo({ avatar, bio, followers, login, name, company })
+  }
+
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
   return (
     <ProfileInfoContainer>
-      <ProfileImg
-        src="https://avatars.githubusercontent.com/u/4248081?v=4"
-        alt=""
-      />
+      <ProfileImg src={userInfo?.avatar} alt="" />
 
       <div>
         <UserNameAndGit>
-          <strong>Cameron Williamson</strong>
+          <strong>{userInfo?.name}</strong>
           <a href="">
             Github <ArrowSquareOut size={20} weight="bold" color={theme.blue} />
           </a>
         </UserNameAndGit>
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{userInfo?.bio}</p>
         <ProfileDetails>
           <span>
             <GithubLogo size={20} weight="fill" color={theme.label} />
-            cameronwll
+            {userInfo?.login}
           </span>
 
           <span>
             <Buildings size={20} weight="fill" color={theme.label} />
-            Rocketseat
+            {userInfo?.company}
           </span>
 
           <span>
             <Users size={20} weight="fill" color={theme.label} />
-            32 seguidores
+            {userInfo?.followers} seguidores
           </span>
         </ProfileDetails>
       </div>
